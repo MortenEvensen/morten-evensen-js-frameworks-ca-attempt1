a35e0e6ca10ddb8fa7f8270ad5fb9b9dd2c2473c
MortenEvensen <evensenmorten@hotmail.com>
2020-06-01 10:05:44 +0200

    Bugfix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Home from "./Components/HomePage"
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+       
         <Home />
     </div>
   );
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Api from "./Content/Api"
 
+
 function Home() {
     return (
         <>
